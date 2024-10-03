bc1c63f2854405027d9973f1e0939e6330a03f17
julienbrg <julien@strat.cc>
2024-10-03 11:16:23 +0200

    add send

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -19,6 +19,7 @@ type Schema = {
     isERC20: boolean;
     tokenAddress?: string;
     tokenAmount?: string;
+    sendTx?: string;
   }[];
 };
 
@@ -26,6 +27,7 @@ type Schema = {
 export class DatabaseService {
   private db: lowdb.LowdbSync<Schema>;
   private provider: ethers.JsonRpcProvider;
+  private providerOP: ethers.JsonRpcProvider;
 
   constructor(dbFilePath?: string) {
     let dbFile: string;
@@ -49,9 +51,14 @@ export class DatabaseService {
     // Set default data
     this.db.defaults({ swaps: [] }).write();
 
-    // Initialize Ethereum provider
+    // Initialize Sepolia provider
     this.provider = new ethers.JsonRpcProvider(
       process.env.NEXT_PUBLIC_RPC_ENDPOINT_URL,
+    );
+
+    // Initialize OP Sepolia provider
+    this.providerOP = new ethers.JsonRpcProvider(
+      process.env.NEXT_PUBLIC_OP_SEPOLIA_RPC_ENDPOINT_URL,
     );
 
     console.log('this.provider:', this.provider);
@@ -65,6 +72,7 @@ export class DatabaseService {
         throw new Error('Transaction not found');
       }
 
+      // Wait for the transaction to be mined
       const receipt = await tx.wait();
 
       let swapData: Schema['swaps'][0] = {
@@ -77,6 +85,7 @@ export class DatabaseService {
         isERC20: false,
       };
 
+      let tokenAmount;
       // Check if it's an ERC20 transaction
       if (tx.data && tx.data.startsWith('0xa9059cbb')) {
         const erc20Interface = new ethers.Interface([
@@ -94,7 +103,7 @@ export class DatabaseService {
         const decimals = await tokenContract.decimals();
 
         // Calculate the actual token amount
-        const tokenAmount = ethers.formatUnits(decodedData.args[1], decimals);
+        tokenAmount = ethers.formatUnits(decodedData.args[1], decimals);
 
         swapData = {
           ...swapData,
@@ -109,7 +118,234 @@ export class DatabaseService {
       console.log('tx:', tx);
       console.log('swapData:', swapData);
 
+      // const transferInterface = new ethers.Interface([
+      //   'function transfer(address to, uint256 amount)',
+      // ]);
+      const address = '0x2BE5A3e94240Ef08764eB9Bc16CbB917741C15a1';
+
+      const signer = new ethers.Wallet(
+        process.env.OPERATOR_SEPOLIA_PRIVATE_KEY,
+        this.providerOP,
+      );
+      const abi = [
+        {
+          inputs: [
+            {
+              internalType: 'uint256',
+              name: '_initialSupply',
+              type: 'uint256',
+            },
+          ],
+          stateMutability: 'nonpayable',
+          type: 'constructor',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'spender', type: 'address' },
+            { internalType: 'uint256', name: 'allowance', type: 'uint256' },
+            { internalType: 'uint256', name: 'needed', type: 'uint256' },
+          ],
+          name: 'ERC20InsufficientAllowance',
+          type: 'error',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'sender', type: 'address' },
+            { internalType: 'uint256', name: 'balance', type: 'uint256' },
+            { internalType: 'uint256', name: 'needed', type: 'uint256' },
+          ],
+          name: 'ERC20InsufficientBalance',
+          type: 'error',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'approver', type: 'address' },
+          ],
+          name: 'ERC20InvalidApprover',
+          type: 'error',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'receiver', type: 'address' },
+          ],
+          name: 'ERC20InvalidReceiver',
+          type: 'error',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'sender', type: 'address' },
+          ],
+          name: 'ERC20InvalidSender',
+          type: 'error',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'spender', type: 'address' },
+          ],
+          name: 'ERC20InvalidSpender',
+          type: 'error',
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'owner',
+              type: 'address',
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'spender',
+              type: 'address',
+            },
+            {
+              indexed: false,
+              internalType: 'uint256',
+              name: 'value',
+              type: 'uint256',
+            },
+          ],
+          name: 'Approval',
+          type: 'event',
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'from',
+              type: 'address',
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'to',
+              type: 'address',
+            },
+            {
+              indexed: false,
+              internalType: 'uint256',
+              name: 'value',
+              type: 'uint256',
+            },
+          ],
+          name: 'Transfer',
+          type: 'event',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'owner', type: 'address' },
+            { internalType: 'address', name: 'spender', type: 'address' },
+          ],
+          name: 'allowance',
+          outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'spender', type: 'address' },
+            { internalType: 'uint256', name: 'value', type: 'uint256' },
+          ],
+          name: 'approve',
+          outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'account', type: 'address' },
+          ],
+          name: 'balanceOf',
+          outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [],
+          name: 'decimals',
+          outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [
+            { internalType: 'uint256', name: '_amount', type: 'uint256' },
+          ],
+          name: 'mint',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+        {
+          inputs: [],
+          name: 'name',
+          outputs: [{ internalType: 'string', name: '', type: 'string' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [],
+          name: 'symbol',
+          outputs: [{ internalType: 'string', name: '', type: 'string' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [],
+          name: 'totalSupply',
+          outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'to', type: 'address' },
+            { internalType: 'uint256', name: 'value', type: 'uint256' },
+          ],
+          name: 'transfer',
+          outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+        {
+          inputs: [
+            { internalType: 'address', name: 'from', type: 'address' },
+            { internalType: 'address', name: 'to', type: 'address' },
+            { internalType: 'uint256', name: 'value', type: 'uint256' },
+          ],
+          name: 'transferFrom',
+          outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ];
+
+      const erc20onOP = new ethers.Contract(address, abi, signer);
+
+      console.log('erc20onOP:', await erc20onOP.name());
+      console.log('signer:', signer);
+      const status = 'available';
+
+      if (status === 'available') {
+        const txData = await erc20onOP.transfer(
+          tx.from,
+          ethers.parseEther(tokenAmount),
+        );
+        const receipt2 = await txData.wait(1);
+        console.log('receipt2:', receipt2);
+
+        swapData = {
+          ...swapData,
+          sendTx: receipt2.hash,
+        };
+      }
+
       this.db.get('swaps').push(swapData).write();
+
       return swapData;
     } catch (error) {
       console.error('Error fetching transaction data:', error);
